0ccb0f50a0e62a657aede9ce62a185a7921de49f
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-04-25 22:45:16 +0200

    Add project files.

---
+++ Konstruktorerövning2inl/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Konstruktorerövning2inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            namn jag = new namn("alex", "ved");
+            namn pappa = new namn("mag", "ved");
+            Console.WriteLine(jag.getFullnamn());
+        }
+    }
+}
---
+++ Konstruktorerövning2inl/namn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktorerövning2inl
+{
+    class namn
+    {
+        private string förnamn;
+        private string efternamn;
+        private string fullnamn;
+
+        public namn(string F, string E) 
+        {
+            förnamn = F;
+            efternamn = E;
+            fullnamn = förnamn + " " + efternamn;
+        }
+        public string getFullnamn() { return fullnamn; }  
+    }
+}
